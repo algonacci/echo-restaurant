0201d85582ddd787f9e9b092961df7df86f2bc65
Eric Julianto <algonacci1@gmail.com>
2023-07-30 18:36:24 +0700

    Update session.go

--- internal/repository/user/session.go
+++ internal/repository/user/session.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"errors"
 	"time"
 
 	"github.com/algonacci/echo-restaurant/internal/model"
@@ -16,19 +17,42 @@ func (ur *userRepo) CreateUserSession(userID string) (model.UserSession, error) 
 	if err != nil {
 		return model.UserSession{}, err
 	}
+
 	return model.UserSession{
 		JWTToken: accessToken,
 	}, nil
+}
+
+func (ur *userRepo) CheckSession(data model.UserSession) (userID string, err error) {
+
+	accessToken, err := jwt.ParseWithClaims(data.JWTToken, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return &ur.signKey.PublicKey, nil
+	})
+	if err != nil {
+		return "", err
+	}
+	accessTokenClaims, ok := accessToken.Claims.(*Claims)
+	if !ok {
+		return "", errors.New("unauthorized")
+	}
+
+	if accessToken.Valid {
+		return accessTokenClaims.Subject, nil
+	}
+
+	return "", errors.New("unauthorized")
 }
 
 func (ur *userRepo) generateAccessToken(userID string) (string, error) {
 	accessTokenExp := time.Now().Add(ur.accessExp).Unix()
 	accessClaims := Claims{
 		jwt.StandardClaims{
-			Subject:   userID,
 			ExpiresAt: accessTokenExp,
+			Subject:   userID,
 		},
 	}
-	accessJwt := jwt.NewWithClaims(jwt.GetSigningMethod("RSA256"), accessClaims)
+
+	accessJwt := jwt.NewWithClaims(jwt.GetSigningMethod("RS256"), accessClaims)
+
 	return accessJwt.SignedString(ur.signKey)
 }
